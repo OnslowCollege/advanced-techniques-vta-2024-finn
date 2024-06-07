d829fa89768a418ed1131646b3c1ae78b85dc6f6
Tinn2 <124308211+Tinn2@users.noreply.github.com>
2024-06-07 02:34:47 +0000

    Jun 7, 2024, 2:34 AM

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+general_su
+
+
+
 while True:
     print("Welcome to Math Test Masterming")
     print(
-        "Choose a topic to work on: \n - (G)eneral Subjects Quiz \n - (A)lgebra \n - (P)robability \n - (B)asic Arithmetic \n - (Q)uit",
+        "Choose a topic to work on: \n - (G)eneral Topics Quiz \n - (A)lgebra \n - (P)robability \n - (B)asic Arithmetic \n - (Q)uit",
         "\n - (Q)uit ",
     )
 
     topic = input("What topic would you like to study: ")
     if topic.upper() == "G":
-        general_subjects_quiz()
+        general_topics_quiz()
     elif topic.upper() == "A":
         algebra()
     elif topic.upper() == "P":
