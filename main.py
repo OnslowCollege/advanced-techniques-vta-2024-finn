c292c7557a8c6e4c7bff3dac3cac2a0f4107d601
Tinn2 <124308211+Tinn2@users.noreply.github.com>
2024-06-14 02:46:53 +0000

    Jun 14, 2024, 2:46 AM

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 """Math Test Mastermind."""
+questions: dict[str, dict[str, str]] = {
+    "Algebra_1": {"2x=4": "x=2"}
+}
+
 
 def general_topics_quiz():
     """Quiz for all topics and levels of those topics."""
     print("Test general topics quiz")
 
-def algebra():
-    """Start Algebra topic."""
+def algebra(Question_info):
+    """
+    Start Algebra topic.
+    
+    Parameters
+    ----------
+    -   Question_info
+    """
     print("Test algebra")
+
 def probability():
     """Start Probability topic."""
     print("Test probability")
