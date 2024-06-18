5788a6cd4f1ff3dd75db54148fe696ff3521e8ca
Tinn2 <124308211+Tinn2@users.noreply.github.com>
2024-06-18 01:20:46 +0000

    Jun 18, 2024, 1:20 AM

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ questions: dict[str, dict[str, str]] = {
     "Algebra_1": {"2x=4": "x=2", "3x=9": "x=3", "x=12": "x=12"},
     "Algebra_2": {"2x-2=6": "x=4"},
     "Algebra_3": {"2x-2=6": "x=4"},
-    "Probability_1": {"1+1=2": "2"},
-    "Probability_2": {"1+1=2": "2"},
-    "Probability_3": {"1+1=2": "2"},    
+    "Fractions_1": {"1+1=2": "2"},
+    "Fractions_2": {"1+1=2": "2"},
+    "Fractions_3": {"1+1=2": "2"},
     "Basic_A_1": {"3+9": "12"},
     "Basic_A_2": {"3+9": "12"},
     "Basic_A_3": {"3+9": "12"},
+    "General_Topics_Quiz": {"3+9": "12"}
 }
 
 
 def general_topics_quiz(Question_info: dict[str, str]):
-    """Quiz for all topics and levels of those topics."""
-    print("Test general topics quiz")
+    """
+    Start All topics quiz.
 
+    Parameters
+    ----------
+    -   Question_info: Gives the questions for the user to answer.
+
+    """
+    print("Test algebra")
 
 
 def algebra(Question_info: dict[str, str]):
@@ -31,7 +38,7 @@ def algebra(Question_info: dict[str, str]):
 
 
 
-def probability(Question_info: dict[str, str]):
+def fractions(Question_info: dict[str, str]):
     """
     Start Algebra topic.
     
@@ -68,11 +75,11 @@ while True:
 
     topic = input("What topic would you like to study: ")
     if topic.upper() == "G":
-        general_topics_quiz()
+        general_topics_quiz(questions["General_Topics_Quiz"])
     elif topic.upper() == "A":
         algebra(questions["Algebra_1"])
     elif topic.upper() == "P":
-        probability(questions["Probability_1"])
+        fractions(questions["Probability_1"])
     elif topic.upper() == "B":
         basic_arithmetic(questions["Basic_A_1"])
     elif topic.upper() == "Q":
