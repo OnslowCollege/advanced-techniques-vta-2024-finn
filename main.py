500b600a67d15f4b8fe61c47c737264895b1a8df
Tinn2 <124308211+Tinn2@users.noreply.github.com>
2024-06-19 23:48:52 +0000

    Jun 19, 2024, 11:48 PM

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 """Math Test Mastermind."""
-questions: dict[str, dict[str, str]] = {
-    "Algebra_1": {"2x=4": "x=2", "3x=9": "x=3", "x=12": "x=12"},
-    "Algebra_2": {"2x-2=6": "x=4"},
-    "Algebra_3": {"4x+3=15": "x=15"},
-    "Fractions_1": {"": "2"},
-    "Fractions_2": {"1+1=2": "2"},
-    "Fractions_3": {"1+1=2": "2"},
-    "Basic_A_1": {"3+9": "12"},
-    "Basic_A_2": {"3+9": "12"},
-    "Basic_A_3": {"3+9": "12"},
-    "General_Topics_Quiz": {"3+9": "12"}
-}
+#questions: dict[str, dict[str, str]] = {
+#    "Algebra_1": {"2x=4": "x=2", "3x=9": "x=3", "x=12": "x=12"},
+#    "Algebra_2": {"2x-2=6": "x=4"},
+#    "Algebra_3": {"4x+3=15": "x=15"},
+#    "Fractions_1": {"": "2"},
+#    "Fractions_2": {"1+1=2": "2"},
+#    "Fractions_3": {"1+1=2": "2"},
+#    "Basic_A_1": {"3+9": "12"},
+#    "Basic_A_2": {"3+9": "12"},
+#    "Basic_A_3": {"3+9": "12"},
+#}
 
 
 def general_topics_quiz(Question_info: dict[str, str]):
