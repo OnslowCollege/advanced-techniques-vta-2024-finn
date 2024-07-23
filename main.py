1bea775760d2ed0b35999a3fef8d35cac041534e
Tinn2 <124308211+Tinn2@users.noreply.github.com>
2024-07-23 21:36:43 +0000

    Jul 23, 2024, 9:36 PM

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 """Math Test Mastermind."""
 questions: dict[str, dict[str, str]] = {
-    "Algebra_1": {"If 2x=4 what is x?": "2", "If 3x=9 what is x?": "x=3", "If x=12 what is x?": "x=12"},
+    "Algebra_1": {"If 2x=4 what is x?": "2", "If 3x=9 what is x?": "3", "If x=12 what is x?": "12"},
     "Algebra_2": {"If 2x-2=6 what is x?": "4"},
-    "Algebra_3": {"4x+3=15": "x=3"},
+    "Algebra_3": {"4x+3=15": "3"},
     "Fractions_1": {"1/2+1/2": "1", "1/3+1/3": "2/3"},
-    "Fractions_2": {"2/3x1/3": "2/9"},
+    "Fractions_2": {"2/3*1/3": "2/9"},
     "Fractions_3": {"11/3÷2/3": "5/2"},
     "Basic_A_1": {"3+9": "12"},
-    "Basic_A_2": {"4x7": "28"},
-    "Basic_A_3": {"8x7+2": "58"},
+    "Basic_A_2": {"4*7": "28"},
+    "Basic_A_3": {"8*7+2": "58"},
 }
 
 
@@ -18,33 +18,33 @@ def general_topics_quiz(hearts):
 
     Parameters
     ----------
-    -   Question_info: Gives the questions for the user to answer.
+    -   hearts: Current number of hearts the user has.
 
     """
     print("Test general topics quiz")
     correct_answers = 0
-    for question in Question_info:
-        answer = questions[question]
-        user_answer = input(f"{question}:  ")
-        if user_answer == answer:
-            print("Correct!")
-            correct_answers += 1
-            if hearts < 5:
-                hearts += 1
-        else:
-            print(f"Incorrect. The correct answer is: {answer}")
-            hearts -= 1
-        if hearts <= 0:
-            print("You have no more hearts left. Game over!")
-            break
+    for topic, Question_info in questions.items():
+        for question, answer in Question_info.items():
+            user_answer = input(f"{question}:  ")
+            if user_answer == answer:
+                print("Correct!")
+                correct_answers += 1
+                if hearts < 5:
+                    hearts += 1
+            else:
+                print(f"Incorrect. The correct answer is: {answer}")
+                hearts -= 1
+            if hearts <= 0:
+                print("You have no more hearts left. Game over!")
+                return hearts
 
-
-
+    print(f"You got {correct_answers} questions correct!")
+    return hearts
 
 
 def algebra(Question_info: dict[str, str], hearts: int) -> int:
     """
-    Start Fractions topic.
+    Start Algebra topic.
     
     Parameter
     ---------
@@ -57,8 +57,7 @@ def algebra(Question_info: dict[str, str], hearts: int) -> int:
 
     """
     correct_answers = 0
-    for question in Question_info:
-        answer = Question_info[question]
+    for question, answer in Question_info.items():
         user_answer = input(f"{question}:  ")
         if user_answer == answer:
             print("Correct!")
@@ -92,8 +91,7 @@ def fractions(Question_info: dict[str, str], hearts: int) -> int:
     """
     print("Fractions Quiz")
     correct_answers = 0
-    for question in Question_info:
-        answer = Question_info[question]
+    for question, answer in Question_info.items():
         user_answer = input(f"{question} = ")
         if user_answer == answer:
             print("Correct!")
@@ -111,19 +109,23 @@ def fractions(Question_info: dict[str, str], hearts: int) -> int:
     return hearts
 
 
-def basic_arithmetic(Question_info: dict[str, str]):
+def basic_arithmetic(Question_info: dict[str, str], hearts: int) -> int:
     """
     Start Basic Arithmetic topic.
     
     Parameters
     ----------
     -   Question_info: Gives the questions for the user to answer.
+    -   hearts: Current number of hearts the user has.
+
+    Return
+    ------
+    - New number of hearts.
 
     """
     print("Basic Arithmetic Quiz")
     correct_answers = 0
-    for question in Question_info:
-        answer = Question_info[question]
+    for question, answer in Question_info.items():
         user_answer = input(f"{question} = ")
         if user_answer == answer:
             print("Correct!")
@@ -137,6 +139,8 @@ def basic_arithmetic(Question_info: dict[str, str]):
             print("You have no more hearts left. Game over!")
             break
 
+    print(f"You got {correct_answers} out of {len(Question_info)} questions correct!")
+    return hearts
 
 
 def select_level(topic):
