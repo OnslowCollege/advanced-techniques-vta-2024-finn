7b6534ceaf083932bcd622827f54346f3e1b60fe
Tinn2 <124308211+Tinn2@users.noreply.github.com>
2024-08-07 23:35:11 +0000

    Aug 7, 2024, 11:35 PM

--- main.py
+++ main.py
@@ -146,8 +146,9 @@ def fractions(question_info: dict[str, str], hearts: int) -> int:
             if hearts < 5:
                 hearts += 1
         else:
-            print(f"Incorrect. The correct answer is: {answer}")
             hearts -= 1
+            print(f"Incorrect. The correct answer is: {answer},"
+                  "test hearts: {hearts}")
         if hearts <= 0:
             print("You have no more hearts left. Game over!")
             break
@@ -182,8 +183,9 @@ def basic_arithmetic(question_info: dict[str, str], hearts: int) -> int:
             if hearts < 5:
                 hearts += 1
         else:
-            print(f"Incorrect, the correct answer is: {answer}")
             hearts -= 1
+            print(f"Incorrect. The correct answer is: {answer},"
+                  "test hearts: {hearts}")
         if hearts <= 0:
             print("You have no more hearts left. Game over!")
             break
