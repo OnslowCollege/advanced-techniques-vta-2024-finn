24c9e6cf00bb634cce9114f97b1ef800470d4d56
Tinn2 <124308211+Tinn2@users.noreply.github.com>
2024-08-07 23:44:15 +0000

    Aug 7, 2024, 11:44 PM

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ def basic_arithmetic(question_info: dict[str, str], hearts: int) -> int:
                 hearts += 1
         else:
             hearts -= 1
-            print(f"Incorrect. The correct answer is: {answer}, "),
+            print(f"Incorrect. The correct answer is: {answer},")
             print(f"Test hearts: {hearts}")
         if hearts <= 0:
             print("You have no more hearts left. Game over!")
